02433923cc170e8a23210e6d8bf8a7d2730504cf
Arun <arun@gmail.com>
2019-04-17 12:58:13 +0530

    common data table changes added

--- src/app/common/data-table/data-table.component.ts
+++ src/app/common/data-table/data-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Output, EventEmitter, Input } from '@angular/core';
+import { MatPaginator, MatTableDataSource } from '@angular/material';
+import { ColumnHeader } from "../../shared/constant/table-column.const";
 
 @Component({
   selector: 'app-data-table',
@@ -7,9 +9,39 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 
 export class DataTableComponent implements OnInit {
-  constructor() { }
+  @Output() eventForCreate = new EventEmitter();
+  @Input() dataList: any;
+  displayedColumns: string[] = [];
+  _columnHeader: any = ColumnHeader
+  dataSource: any;
+  displayValue: any = {};
 
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  constructor() { }
   ngOnInit() {
+    this.displayedColumns = Object.keys(this.dataList[0]);
+    this.dataSource = new MatTableDataSource<any>(this.dataList)
+    this.dataSource.paginator = this.paginator;
+    for (let x in this.displayedColumns) {
+      this.displayValue[this.displayedColumns[x]] = this._columnHeader[this.displayedColumns[x]]
+    }
   }
 
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  showCreate() {
+    this.eventForCreate.emit(true)
+  }
 }
+
+// export interface PeriodicElement {
+//   name: string;
+//   position: string;
+//   weight: number;
+//   symbol: string;
+// }
---
+++ src/app/shared/constant/table-column.const.ts
@@ -0,0 +1,6 @@
+export const ColumnHeader = {
+    position: "Position",
+    name: "Name",
+    weight: "Weight",
+    symbol: "Symbol"
+}
